a975a87c425d8c86b59cd86c2bbc0a6f7cc1283f
Keita Nakagami <keita00@gmail.com>
2020-02-16 14:44:47 +0900

    自動テストスクリプト test.sh

--- 9cc.c
+++ 9cc.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+/*
     if ( argc != 2 ) {
-        fprintf( stderr, "引数の個数が正しくありません");
+        fprintf( stderr, "引数の個数が正しくありません\n");
         return 1;
     }
+*/
 
     printf (".intel_syntax noprefix\n");
     printf(".global main\n");
